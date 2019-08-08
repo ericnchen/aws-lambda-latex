821c272f7d3ab4b05350a49d89abe122ca8954f9
Eric Chen <eric@ericnchen.com>
2019-08-08 22:15:29 +0200

    Move lambdalatex python code into lambdalatex package.

---
+++ lambdalatex/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from .lambda_function import lambda_handler
--- localtest.py
+++ localtest.py
@@ -4,7 +4,7 @@
 import base64
 import pathlib
 
-import lambda_function
+from lambdalatex import lambda_function
 
 
 def localtest(event, context):
