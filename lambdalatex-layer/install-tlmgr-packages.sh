8aad95f90b98c507e83c2177824f5be5711b6f38
Eric Chen <eric@ericnchen.com>
2019-08-09 15:49:37 +0200

    Factor out tlmgr install into its own script.

---
+++ lambdalatex-layer/install-tlmgr-packages.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+# /opt/bin and /opt/lib are appended last but need to be first else wrong perl is used.
+export PATH=/opt/bin:$PATH
+export LD_LIBRARY_PATH=/opt/lib:$LD_LIBRARY_PATH
+
+# Install packages as needed.
+tlmgr install latexmk
+
+#RUN tlmgr install \
+#            latexmk \
+#            xcolor \
+##            tcolorbox \
+#            pgf \
+##            environ \
+##            trimspaces \
+#            etoolbox \
+#            booktabs \
+##            lastpage \
+##            pgfplots \
+##            marginnote \
+##            tabu \
+##            varwidth \
+##            makecell \
+##            enumitem \
+##            setspace \
+##            xwatermark \
+##            catoptions \
+##            ltxkeys \
+#            framed \
+##            parskip \
+##            endnotes \
+##            footmisc \
+##            zapfding \
+##            symbol \
+##            lm \
+##            sectsty \
+##            stringstrings \
+##            koma-script \
+##            multirow \
+##            calculator \
+##            adjustbox \
+#            xkeyval \
+##            collectbox \
+##            siunitx \
+##            l3kernel \
+##            l3packages \
+##            helvetic \
+##            charter \
+#            memoir \
+#            ifetex \
+#            layouts \
+#            glossaries \
+#            mfirstuc \
+#            textcase \
+#            xfor \
+#            datatool \
+#            substr \
+#            fp \
+#            fancyvrb \
+#            tabulary \
+#            ec \
+#            listings \
+#            ulem \
+#            soul \
+#            xargs \
+#            todonotes \
+#            beamer \
+#            translator \
+#            patchcmd \
+#            acronym \
+#            bigfoot \
+#            xstring \
+#            subfigure \
+#            microtype \
+#            && \
+##    tlmgr remove --force luatex && \
+#    rm -f /opt/texlive/2019/tlpkg/texlive.tlpdb* && \
+#    rm -f /opt/texlive/2019/tlpkg/translations/*.po
+##           /opt/texlive/2019/texmf-dist/source/latex/koma-script/doc \
+##           /opt/texlive/2019/texmf-dist/doc
--- lambdalatex-layer/texlive.profile
+++ lambdalatex-layer/texlive.profile
@@ -12,7 +12,7 @@ TEXMFVAR /tmp/.texlive2019/texmf-var
 binary_x86_64-linux 1
 collection-basic 1
 collection-latex 1
-instopt_adjustpath 0
+instopt_adjustpath 1
 instopt_adjustrepo 1
 instopt_letter 0
 instopt_portable 0
@@ -26,7 +26,7 @@ tlpdbopt_generate_updmap 0
 tlpdbopt_install_docfiles 0
 tlpdbopt_install_srcfiles 0
 tlpdbopt_post_code 1
-tlpdbopt_sys_bin /usr/local/bin
-tlpdbopt_sys_info /usr/local/share/info
-tlpdbopt_sys_man /usr/local/share/man
+tlpdbopt_sys_bin /opt/bin
+tlpdbopt_sys_info /opt/info
+tlpdbopt_sys_man /opt/man
 tlpdbopt_w32_multi_user 0
