75dc864d8b573fe07f30a6e6e95e2630882c8793
Eric Chen <eric@ericnchen.com>
2019-08-10 19:40:22 +0200

    Add basic logging (needs work)

--- lambdalatex/lambda_function.py
+++ lambdalatex/lambda_function.py
@@ -2,30 +2,40 @@
 import base64
 import io
 import json
+import logging
 import os
 import pathlib
 import subprocess
 import tempfile
 import zipfile
 
+logger = logging.getLogger(__name__)
+
 STATUS_SUCCESS_PDF_GENERATED = 200
 
 
 # noinspection PyUnusedLocal
 def lambda_handler(event, context):
-    body = get_body(event)
+    logger.info(f"event has the following keys: {', '.join(list(event.keys()))}")
 
     td = tempfile.TemporaryDirectory()
+    logger.info(f"created temporary directory at {td.name}")
 
     unzip_dir = pathlib.Path(td.name)
+    body = get_body(event)
     extract_zipfile(get_zipfile(body), target=unzip_dir)
 
     infile = unzip_dir / "main.tex"
     pdf = infile.with_suffix(".pdf")
 
+    logger.info(f"PATH is currently: {os.environ['PATH']}")
     os.environ["PATH"] = "/opt/bin:" + os.environ["PATH"]
-    cmd = ["latexmk", "-verbose", "-interaction=batchmode", "-pdf", infile]
+    logger.info(f"PATH is now: {os.environ['PATH']}")
+
+    cmd = ["latexmk", "-verbose", "-interaction=batchmode", "-pdf", str(infile)]
+    logger.info(f"calling subprocess with command: {' '.join(cmd)}")
     r = subprocess.run(cmd, cwd=unzip_dir, encoding="utf-8", capture_output=True)
+    logger.info(f"subprocess return code is {r.returncode}")
 
     output_body = {
         "pdf": get_pdfstr(pdf),
@@ -34,6 +44,7 @@ def lambda_handler(event, context):
     }
 
     td.cleanup()
+    logger.info("temporary directory has been removed")
 
     return {
         "body": json.dumps(output_body),
@@ -60,12 +71,18 @@ def get_pdfstr(filename: str) -> str:
 
 def extract_zipfile(zip_file: zipfile.ZipFile, target: pathlib.Path):
     """Extract the zipfile to the target directory."""
+    logger.info(f"unzipping {zip_file.filename} to {str(target)}")
     zip_file.extractall(path=target)
+    logger.info(f"unzipped {zip_file.filename} to {str(target)}")
 
 
 def get_body(event: dict) -> dict:
     """Get the event body from the lambda handler."""
-    return json.loads(event["body"]) if "body" in event else event
+    if 'body' in event:
+        logger.info("lambda_handler called from API Gateway")
+        return json.loads(event["body"])
+    logger.info("lambda_handler called from local Docker image")
+    return event
 
 
 def get_zipfile(body: dict) -> zipfile.ZipFile:
