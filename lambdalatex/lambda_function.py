ffdf5801a4ef3cc741acaa95a78ef605fc4eb845
Eric Chen <eric@ericnchen.com>
2019-08-10 16:50:23 +0200

    Fix input extraction when called from API Gateway.

--- lambdalatex/lambda_function.py
+++ lambdalatex/lambda_function.py
@@ -13,31 +13,29 @@ STATUS_SUCCESS_PDF_GENERATED = 200
 
 # noinspection PyUnusedLocal
 def lambda_handler(event, context):
-    """
-    Events:
-        main_filename: name of the main latex file to compile (defaults to main.tex)
-    """
+    body = get_body(event)
+
     td = tempfile.TemporaryDirectory()
 
     unzip_dir = pathlib.Path(td.name)
-    extract_zipfile(get_zipfile(event), target=unzip_dir)
+    extract_zipfile(get_zipfile(body), target=unzip_dir)
 
-    infile = unzip_dir / event.get("main_filename", "main.tex")
+    infile = unzip_dir / "main.tex"
     pdf = infile.with_suffix(".pdf")
 
     os.environ["PATH"] = "/opt/bin:" + os.environ["PATH"]
     cmd = ["latexmk", "-verbose", "-interaction=batchmode", "-pdf", infile]
     r = subprocess.run(cmd, cwd=unzip_dir, encoding="utf-8", capture_output=True)
 
-    body = {"pdf": get_pdfstr(pdf), "stdout": get_stdout(r), "stderr": get_stderr(r)}
+    output_body = {"pdf": get_pdfstr(pdf), "stdout": get_stdout(r), "stderr": get_stderr(r)}
 
     td.cleanup()
 
     return {
-        "body": json.dumps(body),
+        "body": json.dumps(output_body),
         "headers": {"Content-Type": "application/json"},
         "isBase64Encoded": False,
-        "statusCode": STATUS_SUCCESS_PDF_GENERATED if body["pdf"] != "" else 500,
+        "statusCode": STATUS_SUCCESS_PDF_GENERATED if output_body["pdf"] != "" else 500,
     }
 
 
@@ -61,6 +59,11 @@ def extract_zipfile(zip_file: zipfile.ZipFile, target: pathlib.Path):
     zip_file.extractall(path=target)
 
 
-def get_zipfile(event):
+def get_body(event: dict) -> dict:
+    """Get the event body from the lambda handler."""
+    return json.loads(event['body']) if 'body' in event else event
+
+
+def get_zipfile(body: dict) -> zipfile.ZipFile:
     """Get input as a zipfile from event trigger."""
-    return zipfile.ZipFile(io.BytesIO(base64.b64decode(event["input"])))
+    return zipfile.ZipFile(io.BytesIO(base64.b64decode(body["input"])))
