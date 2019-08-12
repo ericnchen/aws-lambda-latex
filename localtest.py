ea79c86c80229df31680aba3c910d91296c7500a
Eric Chen <eric@ericnchen.com>
2019-08-12 19:45:37 +0200

    Refactor localtest to be more simple

--- localtest.py
+++ localtest.py
@@ -5,10 +5,7 @@ import base64
 import json
 import os
 import pathlib
-import shutil
 import subprocess
-import tempfile
-import uuid
 import zipfile
 
 import click
@@ -16,110 +13,94 @@ import click
 
 @click.command()
 @click.option(
-    "--input",
-    "input_dir",
-    type=click.Path(exists=True),
+    "-i",
+    "--input_dir",
+    type=click.Path(exists=True, file_okay=False),
     required=True,
-    help="path to an input directory (e.g., examples/article)",
+    help="input directory to zip up and use",
 )
 @click.option(
+    "-o",
     "--output_dir",
-    "output_dir",
-    type=click.Path(),
-    default=str(pathlib.Path.cwd()),
+    type=click.Path(file_okay=False),
+    default=str(pathlib.Path().cwd() / "output"),
     help="directory to write output files to",
     show_default=True,
 )
 @click.option(
-    "--layer",
-    type=click.File("rb"),
-    default="lambdalatex-layer/lambdalatex-layer.zip",
-    show_default=True,
-    help="path to the layer file to use",
+    "--compile-with",
+    default="docker",
+    type=click.Choice(["docker", "aws", "native"]),
+    show_default="docker",
 )
-def main(input_dir, output_dir, layer):
-    # Transform inputs.
-    input_path = pathlib.Path(input_dir)
-    layer_zipfile_path = pathlib.Path(layer.name).resolve()
+def main(input_dir, output_dir, compile_with):
+    input_dir = pathlib.Path(input_dir).resolve()
+    output_dir = pathlib.Path(output_dir).resolve()
+    layer_zipfile = pathlib.Path("lambdalatex-layer/lambdalatex-layer.zip").resolve()
 
-    output_dir = pathlib.Path(output_dir).absolute()
-    output_dir.mkdir(exist_ok=True, parents=True)
-    pdf_fp = output_dir / "out.pdf"
-    stdout_fp = pdf_fp.with_suffix(".stdout.txt")
-    stderr_fp = pdf_fp.with_suffix(".stderr.txt")
-    stdout_latexmk_fp = pdf_fp.with_suffix(".stdout.latexmk.txt")
-    stderr_latexmk_fp = pdf_fp.with_suffix(".stderr.latexmk.txt")
+    layer_opt_dir = output_dir / "opt"
+    layer_opt_dir.mkdir(exist_ok=True, parents=True)
+    unzip_zipfile(layer_zipfile, layer_opt_dir, replace=True)
 
-    click.echo("Running local test with the following arguments:")
-    click.echo(f"    Input     {input_dir}")
-    click.echo(f"    Output    out.pdf")
-    click.echo(f"    Layer     {layer.name}")
+    input_zipfile = zip_input_dir(input_dir=input_dir, output_dir=output_dir)
+    input_zipfile_b64 = base64.b64encode(input_zipfile.read_bytes()).decode("utf-8")
 
-    layer_path = make_layer_dir()
-    unzip_layer(layer_path, layer_zipfile_path)
+    event = json.dumps({"body": {"input": input_zipfile_b64}})
+    context = json.dumps({})
 
-    r = subprocess.run(
-        [
+    if compile_with == "docker":
+        command = [
             "docker",
             "run",
             "--rm",
             "-v",
-            f"{layer_path}:/opt",
+            f"{layer_opt_dir}:/opt",
             "-v",
             f"{pathlib.Path.cwd()}:/var/task",
             "lambci/lambda:python3.7",
             "handler.lambda_handler",
-            json.dumps({"body": {"input": make_input_str(input_path)}}),
-            json.dumps({}),
-        ],
-        encoding="utf-8",
-        capture_output=True,
-    )
+            event,
+            context,
+        ]
+    else:
+        raise NotImplementedError
 
-    # test outputs
-    stdout_dict = json.loads(r.stdout)
-    lambda_body = stdout_dict.pop("body")
-    lambda_response = json.loads(lambda_body)
+    r = subprocess.run(command, encoding="utf-8", capture_output=True)
 
-    stdout_fp.write_text(json.dumps(stdout_dict))
-    stderr_fp.write_text(r.stderr)
-    if "stdout" in lambda_response:
-        stdout_latexmk_fp.write_text(lambda_response["stdout"])
-    if "stderr" in lambda_response:
-        stdout_latexmk_fp.write_text(lambda_response["stderr"])
-    if "output" in lambda_response:
-        pdf_fp.write_bytes(base64.b64decode(lambda_response["output"]))
+    if r.stderr != "":
+        (output_dir / "_stderr.txt").write_text(r.stderr)
 
-    # Clean up temporary directory.
-    shutil.rmtree(layer_path, ignore_errors=True)
+    response_body = json.loads(json.loads(r.stdout)["body"])
+
+    for k in ("stdout", "stderr"):
+        if response_body.get(k, "") != "":
+            (output_dir / k).with_suffix(".txt").write_text(response_body[k])
+
+    if "output" in response_body:
+        decoded_pdf = base64.b64decode(response_body["output"])
+        (output_dir / "output.pdf").write_bytes(decoded_pdf)
+        click.echo(f"lambdalatex: saved pdf to {output_dir}")
 
 
-def unzip_layer(layer_path: pathlib.Path, layer_zip_path: pathlib.Path):
+def unzip_zipfile(zipfile_: pathlib.Path, layer: pathlib.Path, replace: bool = True):
     """Call subprocess because extracting via Python removes executable permissions."""
     this_dir = pathlib.Path.cwd()
-    os.chdir(str(layer_path))
-    subprocess.run(["unzip", "-q", str(layer_zip_path)])
+    os.chdir(str(layer))
+    if replace:
+        cmd = ["unzip", "-qo", str(zipfile_)]
+    else:
+        cmd = ["unzip", "-q", str(zipfile_)]
+    subprocess.run(cmd)
     os.chdir(str(this_dir))
-    click.echo(f"Unzipped {layer_zip_path.name} to {layer_path}")
+    click.echo(f"unzipped: {zipfile_.name} to {layer}")
 
 
-def make_layer_dir(layer_dir_prefix: str = "__localtest__/lambdalatex-layer-opt"):
-    """Create and return a path to a temporary directory for our layer."""
-    layer_path = pathlib.Path(f"{layer_dir_prefix}-{uuid.uuid4()}").resolve()
-    layer_path.mkdir(parents=True)
-    return layer_path
-
-
-def make_input_str(input_path: pathlib.Path):
-    """Create a base64 encoded zip file of the input directory."""
-    with tempfile.SpooledTemporaryFile() as tf:
-        with zipfile.ZipFile(tf, "w") as zf:
-            for fn in input_path.iterdir():
-                zf.write(fn, fn.name)
-        # Reset temporary file pointer, read the contents, and base64 encode.
-        tf.seek(0)
-        input_str = base64.b64encode(tf.read()).decode("utf-8")
-    return input_str
+def zip_input_dir(input_dir: pathlib.Path, output_dir: pathlib.Path) -> pathlib.Path:
+    input_zipfile = output_dir / "input.zip"
+    with zipfile.ZipFile(input_zipfile, "w") as zf:
+        for fn in input_dir.iterdir():
+            zf.write(fn, fn.name)
+    return input_zipfile
 
 
 if __name__ == "__main__":
